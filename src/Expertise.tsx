5dda471f0dbb7e62db9d2e9cbf7fc164aba41ce3
Touraj Vaziri <vtouraj@gmail.com>
2018-04-24 16:33:34 -0700

    Fixing the Home Text and position. Added profile image.

---
+++ src/Expertise.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './Expertise.css';
+
+class Expertise extends React.Component {
+    render() {
+        return (
+            <div className="Resume-container">
+                <p className="Resume-centered">My Expertise</p>
+            </div>
+        );
+    }
+}
+
+export default Expertise;
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import './Home.css';
 
-// const logo = require('./backgroundLaptop.jpg');
+const logo = require('./tourajPhoto.png');
 
 class Home extends React.Component {
     render() {
         return (
             <div className="Home-container">
-                {/* <img src={logo} alt="logo" /> */}
-                <p className="Home-centered">Hi, I'm Touraj <br /> A Software Engineer...</p>
+                <p className="Home-centered">Hello! <br /> I'm Touraj <br /> <br /> I am a Software Engineer.</p>
+                <img id="profilepic" src={logo} alt="profile picture"/>
             </div>
         );
     }
