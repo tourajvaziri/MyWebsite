bc22a81584704763f56bbc70d29358ef51478a08
Touraj Vaziri <touraj@vrsquare.ca>
2019-02-05 17:37:59 -0800

    Updating the footer. Github and Linkedin logos.

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -5,15 +5,9 @@ class Resume extends React.Component {
     render() {
         return (
             <section id="sub-footer" className="section">
-                <div className="container">
-                    <div className="row">
-                        <div className="columns">
-                            <p>Get in touch!<br />I am currently accepting new projects.</p>
-                                <div className="cd-modal-action">
-                                    <a href="https://s3.amazonaws.com/aws-website-wwwtourajvaziricom-9naq0/Touraj+Vaziri+Resume.pdf" className="btn" target="_blank">VIEW RESUME</a>
-                                </div>
-                        </div>
-                    </div>
+                <p>Get in touch!<br />I am currently accepting new projects.</p>
+                <div className="cd-modal-action">
+                    <a href="https://s3.amazonaws.com/aws-website-wwwtourajvaziricom-9naq0/Touraj+Vaziri+Resume.pdf" className="btn" target="_blank">VIEW RESUME</a>
                 </div>
             </section>
         );
